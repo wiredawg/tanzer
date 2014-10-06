1ce0a68f09a2b75ddb2ec2a9c72d860d6b132a7b
Xan Tronix <xan@xantronix.net>
2014-10-05 22:45:43 -0500

    Implement exception throwing and handling
    Changes:
    
        * Implement [::tanzer::error throw] to allow throwing of HTTP errors
    
        * Implement [::tanzer::error try] to allow interception of all errors,
          whether generated by [::tanzer::error throw] or not, and to generate
          500 Internal Server Error wrappers as appropriate

--- lib/tanzer/error.tcl
+++ lib/tanzer/error.tcl
@@ -3,15 +3,49 @@ package require tanzer::response
 
 namespace eval ::tanzer::error {
     namespace ensemble create
-    namespace export new response servable
+    namespace export new throw try response servable
 }
 
 proc ::tanzer::error::new {code msg} {
     return [list [namespace current] $code $msg]
 }
 
+proc ::tanzer::error::throw {code msg} {
+    error [::tanzer::error new $code $msg]
+}
+
 proc ::tanzer::error::servable {error} {
     return [expr {[string first "[namespace current] " $error] == 0}]
+}
+
+proc ::tanzer::error::try {script catch ename catchBlock} {
+    set ret {}
+
+    if {$catch ne "catch"} {
+        error "Invalid command invocation"
+    }
+
+    if {[catch {set ret [uplevel 1 $script]} error]} {
+        set type  [lindex $::errorCode 0]
+        set errno [lindex $::errorCode 1]
+        set msg   [lindex $::errorCode 2]
+
+        if {[::tanzer::error servable $error]} {
+            set e $error
+        } else {
+            switch -- $errno ENOTDIR - ENOENT {
+                set e [::tanzer::error new 404 $msg]
+            } EPERM {
+                set e [::tanzer::error new 403 $msg]
+            } default {
+                set e [::tanzer::error new 500 $error]
+            }
+        }
+
+        return [uplevel 1 [join [list [list set $ename $e] $catchBlock] "\n"]]
+    }
+
+    return $ret
 }
 
 proc ::tanzer::error::response {error} {
