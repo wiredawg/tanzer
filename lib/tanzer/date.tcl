ce068e7481ac6bbee7cbcbd7ce623764d866db18
Xan Tronix <xan@xantronix.net>
2014-10-15 19:26:29 -0500

    Implement [::tanzer::date::epoch]

--- lib/tanzer/date.tcl
+++ lib/tanzer/date.tcl
@@ -1,7 +1,13 @@
 package provide tanzer::date 0.0.1
 
-namespace eval ::tanzer::date {}
+namespace eval ::tanzer::date {
+    variable rfc2616Format "%a, %d %b %Y %T %Z"
+}
 
 proc ::tanzer::date::rfc2616 {epoch} {
-    return [clock format $epoch -format "%a, %d %b %Y %T %Z" -gmt 1]
+    return [clock format $epoch -format $::tanzer::date::rfc2616Format -gmt 1]
+}
+
+proc ::tanzer::date::epoch {rfc2616} {
+    return [clock scan $rfc2616 -format $::tanzer::date::rfc2616Format]
 }
