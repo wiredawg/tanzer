61c9c6063b0cd86a20d53c2812df11e75fb9f5bf
Xan Tronix <xan@xantronix.net>
2014-10-28 21:25:54 -0500

    Y U NO GENERATE DOXYGEN MARKUP???

--- lib/tanzer/date.tcl
+++ lib/tanzer/date.tcl
@@ -1,13 +1,27 @@
 package provide tanzer::date 0.1
 
+##
+# @file tanzer/date.tcl
+#
+# Functions for parsing and generating RFC 2616 timestamps
+#
+
 namespace eval ::tanzer::date {
     variable rfc2616Format "%a, %d %b %Y %T %Z"
 }
 
+##
+# Transform a timestamp in seconds since the Unix epoch, provided in `$epoch`,
+# to an RFC 2616 format timestamp.
+#
 proc ::tanzer::date::rfc2616 {epoch} {
     return [clock format $epoch -format $::tanzer::date::rfc2616Format -gmt 1]
 }
 
+##
+# Transform an RFC 2616 timestamp as provided in `$rfc2616` to seconds since
+# the Unix epoch.
+#
 proc ::tanzer::date::epoch {rfc2616} {
     return [clock scan $rfc2616 -format $::tanzer::date::rfc2616Format]
 }
