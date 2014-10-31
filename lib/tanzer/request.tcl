1ea52008056f2b621b02cef4751341715c84ae46
Xan Tronix <xan@xantronix.net>
2014-10-31 02:44:56 -0500

    Add Doxygen to ::tanzer::request

--- lib/tanzer/request.tcl
+++ lib/tanzer/request.tcl
@@ -4,12 +4,16 @@ package require tanzer::date
 package require tanzer::uri
 package require TclOO
 
-namespace eval ::tanzer::request {}
-
+##
+# The HTTP/1.1 request object.
+#
 ::oo::class create ::tanzer::request {
     superclass ::tanzer::message
 }
 
+##
+# Create a request object attached to the session specified in `$newSession`.
+#
 ::oo::define ::tanzer::request constructor {newSession} {
     my variable session env headers buffer config \
         uri path params rewritten timestamp headerLength
@@ -28,6 +32,12 @@ namespace eval ::tanzer::request {}
     set timestamp    [::tanzer::date::rfc2616 [clock seconds]]
 }
 
+##
+# Parse the URI passed in `$newUri` for the URL-decoded path component, and the
+# query string.  These individual components will be parsed and URL decoded as
+# appropriate.  `$newUri` will be saved literally in the current request for
+# future reference.
+#
 ::oo::define ::tanzer::request method uri {{newUri ""}} {
     my variable uri path
 
@@ -57,6 +67,12 @@ namespace eval ::tanzer::request {}
     return
 }
 
+##
+# Match the URI stored in a previous invocation of ::tanezr::requst::uri with
+# the regular expression in `$re`, and replace it with the newly formatted
+# string created from the `[format]` string in `$newFormat`, with the values of
+# subexpression matches given for positional format arguments.
+#
 ::oo::define ::tanzer::request method rewrite {re newFormat} {
     my variable uri rewritten
 
@@ -79,12 +95,35 @@ namespace eval ::tanzer::request {}
     return 1
 }
 
+##
+# Return the URL decoded path component of the URI previously passed to the
+# current request by ::tanzer::request::uri.
+#
 ::oo::define ::tanzer::request method path {} {
     my variable path
 
     return $path
 }
 
+##
+# Returns true if the ::tanzer::route object specified by `$route` matches the
+# current request.  Furthermore, upon a successful match, any positional
+# parameters matching the `:param` format are stored in the current request.
+# The following match criteria are considered, in order:
+#
+# 1. If the regular expression supplied for the HTTP method of `$route` does
+#    not match that of the current request, then return false.
+#
+# 2. If the regular expression supplied for the HTTP host of `$route` does not
+#    match the HTTP `Host:` header of the current request, then return false.
+#
+# 3. If the glob pattern supplied for the HTTP reuqest path of `$route` does
+#    not match the current request path, then return false.
+#
+# As an additional side effect, any path components matching at and beyond the
+# point of a route path glob are stored as a string in the request environment
+# variable `PATH_INFO`, in the same fashion as CGI/1.1.
+#
 ::oo::define ::tanzer::request method matches {route} {
     my variable params path
 
@@ -195,6 +234,10 @@ namespace eval ::tanzer::request {}
     return 1
 }
 
+##
+# Return the value of the parameter called `$name`, or if `$value` is also
+# specified, set the value of `$name` to `$value`.
+#
 ::oo::define ::tanzer::request method param {name {value ""}} {
     my variable params
 
@@ -207,6 +250,11 @@ namespace eval ::tanzer::request {}
     return [dict get $params $name]
 }
 
+##
+# Return a key-value pair list of parameters parsed by a prior call to
+# ::tanzer::request::match.  If `$newParams` is specified, then replace all
+# current parameters with the values therein.
+#
 ::oo::define ::tanzer::request method params {{newParams {}}} {
     my variable params
 
@@ -219,6 +267,12 @@ namespace eval ::tanzer::request {}
     return $params
 }
 
+##
+# Return all the CGI/1.1 environment variables set for the current request as a
+# list of key-value pairs.  If `$name` is supplied, then return only the value
+# of that variable, if it exists.  If `$value` is also supplied, then set the
+# environment variable `$name` with that value.
+#
 ::oo::define ::tanzer::request method env {{name ""} {value ""}} {
     my variable env
 
@@ -237,6 +291,10 @@ namespace eval ::tanzer::request {}
     return $env
 }
 
+##
+# Return the HTTP method verb of the current request, or `-` if one was not
+# parsed nor specified previously.
+#
 ::oo::define ::tanzer::request method method {} {
     my variable env
 
@@ -247,12 +305,19 @@ namespace eval ::tanzer::request {}
     return "-"
 }
 
+##
+# Returns true if no headers are present for the current request.
+#
 ::oo::define ::tanzer::request method empty {} {
     my variable headers
 
     return [expr {[llength $headers] == 0}]
 }
 
+##
+# Returns the value of the `Host:` header for the current request, or an empty
+# string if one is not present.
+#
 ::oo::define ::tanzer::request method host {} {
     if {[my headerExists Host]} {
         return [my header Host]
@@ -261,6 +326,10 @@ namespace eval ::tanzer::request {}
     return {}
 }
 
+##
+# Returns the value of the `REMOTE_ADDR` environment variable set for the
+# current request, or `-` if no such variable is present.
+#
 ::oo::define ::tanzer::request method client {} {
     my variable env
 
@@ -271,6 +340,10 @@ namespace eval ::tanzer::request {}
     return "-"
 }
 
+##
+# Returns the value of the `SERVER_PROTOCOL` environment variable set for the
+# current request, or `-` if no such variable is present.
+#
 ::oo::define ::tanzer::request method proto {} {
     my variable env
 
@@ -281,6 +354,10 @@ namespace eval ::tanzer::request {}
     return "-"
 }
 
+##
+# Returns the value of the `Referer:` header of the current request, or `-` if
+# that header is not present.
+#
 ::oo::define ::tanzer::request method referer {} {
     if {[my headerExists Referer]} {
         return [my header Referer]
@@ -289,6 +366,10 @@ namespace eval ::tanzer::request {}
     return "-"
 }
 
+##
+# Returns the value of the `User-Agent:` header of the current request, or
+# the string `(unknown)` if the header is not present.
+#
 ::oo::define ::tanzer::request method agent {} {
     if {[my headerExists User-Agent]} {
         return [my header User-Agent]
@@ -297,6 +378,10 @@ namespace eval ::tanzer::request {}
     return "(unknown)"
 }
 
+##
+# Return the Unix epoch timestamp corresponding to the data in which the
+# current request object was created.
+#
 ::oo::define ::tanzer::request method timestamp {} {
     my variable timestamp
 
