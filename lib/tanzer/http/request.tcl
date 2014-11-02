562f967408b42a6fb4e45995953ebb7b1406e373
Xan Tronix <xan@xantronix.net>
2014-11-02 01:06:41 -0600

    Write Doxygen stuf for ::tanzer::http::request

--- lib/tanzer/http/request.tcl
+++ lib/tanzer/http/request.tcl
@@ -1,4 +1,11 @@
 package provide tanzer::http::request 0.1
+
+##
+# @file tanzer/http/request.tcl
+#
+# The HTTP request parser facility
+#
+
 package require tanzer::request
 package require tanzer::error
 package require tanzer::uri
@@ -8,10 +15,11 @@ namespace eval ::tanzer::http::request {
     variable proto "http"
 }
 
+##
+# A minimal wrapper to ::tanzer::request.  Provides no additional facilities,
+# as the facilities within ::tanzer::request are sufficiently general for
+# parsing HTTP requests from any source.
+#
 ::oo::class create ::tanzer::http::request {
     superclass ::tanzer::request
-}
-
-::oo::define ::tanzer::http::request constructor {session} {
-    next $session
 }
