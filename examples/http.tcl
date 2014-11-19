7e6d5fb5b1f359c1f55b47c18584d89034424487
Xan Tronix <xan@xantronix.net>
2014-11-18 23:22:38 -0600

    Simplify that

--- examples/http.tcl
+++ examples/http.tcl
@@ -1,7 +1,7 @@
 #! /usr/bin/env tclsh8.6
 
 package require tanzer
-package require tanzer::cgi::handler
+package require tanzer::file::handler
 
 proc usage {} {
     puts stderr "usage: $::argv0 port root"
@@ -18,12 +18,6 @@ set server [::tanzer::server new [list \
     port  $port \
     proto "http" \
 ]]
-
-$server route {.*} /env/* {.*} [::tanzer::cgi::handler new {
-    program "/var/www/xantronix.net/doc/env.cgi"
-    name    "/env"
-    root    "/var/www/xantronix.net/doc"
-}]
 
 $server route {.*} /* {.*} [::tanzer::file::handler new [list \
     root     $root \
