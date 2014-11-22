f3a6bcee35df992aa1addaebb3c3aa753ccdf9d0
Xan Tronix <xan@xantronix.net>
2014-11-22 11:37:00 -0600

    Spiff up examples/scgiclient.tcl somewhat

--- examples/scgiclient.tcl
+++ examples/scgiclient.tcl
@@ -3,13 +3,24 @@
 package require tanzer
 package require tanzer::scgi::handler
 
+proc usage {} {
+    puts stderr "usage: $::argv0 listenport scgihost scgiport"
+    exit 1
+}
+
+if {$argc != 3} {
+    usage
+}
+
+lassign $argv listenport scgihost scgiport
+
 set server [::tanzer::server new]
 
-$server route GET /* {.*} [::tanzer::scgi::handler new {
-    host "localhost"
-    port 1337
-    name "/"
-}]
+$server route GET /* {.*} [::tanzer::scgi::handler new [list \
+    host $scgihost \
+    port $scgiport \
+    name "/"   \
+]]
 
-set listener [socket -server [list $server accept] 8080]
+set listener [socket -server [list $server accept] $listenport]
 vwait forever
