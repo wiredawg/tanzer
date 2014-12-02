0242f14047f2b06220ac69725046745abb7b67b0
Xan Tronix <xan@xantronix.net>
2014-12-02 00:31:12 -0600

    Make ::tanzer require tanzer::error

--- lib/tanzer.tcl
+++ lib/tanzer.tcl
@@ -1,6 +1,7 @@
 package provide tanzer 0.1
 
 package require tanzer::server
+package require tanzer::error
 package require tanzer::http
 package require tanzer::request
 package require tanzer::response
