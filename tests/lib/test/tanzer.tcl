70f3185424d7f905a509a553096b040e5bf9b2fc
Xan Tronix <xan@xantronix.net>
2014-12-04 01:01:21 -0600

    Initial commit of test::tanzer

---
+++ tests/lib/test/tanzer.tcl
@@ -0,0 +1,20 @@
+package provide test::tanzer 0.1
+package require tcltest 2.0
+
+namespace eval ::test::tanzer {}
+
+proc ::test::tanzer::lives {name desc body} {
+    ::tcltest::test $name $desc {
+        catch $body
+    } 0
+}
+
+proc ::test::tanzer::dies {name desc body expected} {
+    ::tcltest::test $name $desc {
+        catch $body err
+    } 0
+
+    ::tcltest::test $name "$name dies with $expected" {
+        regexp $expected $err
+    } 1
+}
