88b611fd2aa420fd62165fd0d56047762c0ffe6d
Xan Tronix <xan@xantronix.net>
2014-12-12 04:32:37 -0600

    Refactor examples/template.tcl to take advantage of new ::teaspoon::handler

--- examples/template.tcl
+++ examples/template.tcl
@@ -1,8 +1,7 @@
 #! /usr/bin/env tclsh8.6
 
 package require tanzer
-package require tanzer::file::handler
-package require teaspoon
+package require teaspoon::handler
 
 proc usage {} {
     puts stderr "usage: $::argv0 port root"
@@ -17,36 +16,12 @@ lassign $::argv port root
 
 set server [::tanzer::server new [list \
     port  $port \
-    proto "http" \
-]]
+    proto "http"]]
 
-$server route {.*} /* {.*} [::tanzer::file::handler new [list \
+$server route {.*} /* {.*} [::teaspoon::handler new [list \
     root     $root \
     listings 1     \
-    index    {index.tsp index.html} \
-    filters  {
-        {.*\.tsp$} {apply {{session localPath st} {
-            set template [::teaspoon open $localPath]
-
-            $session response -new [::tanzer::response new 200 {
-                Content-Type "text/html"
-            }]
-
-            $session respond
-
-            set output [::teaspoon::output new [list $session write]]
-
-            chan push stdout $output
-
-            $session cleanup chan pop stdout
-            $session cleanup $template destroy
-
-            $template process $session
-
-            $session nextRequest
-        }}}
-    } \
-]]
+    index    {index.tsp index.html}]]
 
 set listener [socket -server [list $server accept] $port]
 vwait forever
