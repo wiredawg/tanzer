73a7b4fff0676e11470944fa4f44d4fb9c5c0710
Xan Tronix <xan@xantronix.net>
2014-12-13 09:48:57 -0600

    Refactor ::tanzer::scgi::handler to react to SCGI socket events, not client socket events, for performance

--- lib/tanzer/scgi/handler.tcl
+++ lib/tanzer/scgi/handler.tcl
@@ -51,7 +51,7 @@ namespace eval ::tanzer::scgi::handler {
 # response bodies from the SCGI service to the originating client.
 #
 ::oo::define ::tanzer::scgi::handler constructor {opts} {
-    my variable config socks buffers bodies requested
+    my variable config socks bodies buffers requested
 
     next $opts
 
@@ -63,8 +63,8 @@ namespace eval ::tanzer::scgi::handler {
 
     array set config    {}
     array set socks     {}
-    array set buffers   {}
     array set bodies    {}
+    array set buffers   {}
     array set requested {}
 
     foreach {name message} $requirements {
@@ -82,6 +82,44 @@ namespace eval ::tanzer::scgi::handler {
 # Given the session handler specified in `$session`, serialize the current
 # request into an SCGI protocol request return value.
 #
+::oo::define ::tanzer::scgi::handler method open {session} {
+    my variable config socks bodies buffers requested
+
+    next $session
+
+    set server  [$session server]
+    set request [$session request]
+    set sock    [socket $config(host) $config(port)]
+
+    chan configure $sock    \
+        -translation binary \
+        -blocking    0      \
+        -buffering   full   \
+        -buffersize  [$session config readsize]
+
+    set socks($session)     $sock
+    set bodies($session)    ""
+    set buffers($session)   ""
+    set requested($session) 0
+
+    #
+    # Bind writable events on the SCGI socket to the server's reader handler.
+    #
+    $session reset none
+
+    chan event $sock readable {}
+    chan event $sock writable [list $server respond read [$session sock]]
+
+    #
+    # Now, prepare a new, empty response object, and ensure any session state
+    # is cleaned up when appropriate.
+    #
+    $session response -new [::tanzer::response new \
+        $::tanzer::forwarder::defaultStatus]
+
+    $session cleanup [self] cleanup $session
+}
+
 ::oo::define ::tanzer::scgi::handler method encodeRequest {session} {
     my variable config bodies
 
@@ -117,33 +155,8 @@ namespace eval ::tanzer::scgi::handler {
     return "[string length $data]:$data,"
 }
 
-::oo::define ::tanzer::scgi::handler method open {session} {
-    my variable config socks buffers requested
-
-    next $session
-
-    set request [$session request]
-    set sock    [socket $config(host) $config(port)]
-
-    chan configure $sock    \
-        -translation binary \
-        -blocking    0      \
-        -buffering   full   \
-        -buffersize  [$session config readsize]
-
-    set socks($session)     $sock
-    set buffers($session)   ""
-    set bodies($session)    ""
-    set requested($session) 0
-
-    $session response -new [::tanzer::response new \
-        $::tanzer::forwarder::defaultStatus]
-
-    $session cleanup [self] cleanup $session
-}
-
 ::oo::define ::tanzer::scgi::handler method cleanup {session} {
-    my variable socks buffers bodies requested
+    my variable socks bodies buffers
 
     if {[array get socks $session] eq {}} {
         return
@@ -153,10 +166,9 @@ namespace eval ::tanzer::scgi::handler {
         ::close $socks($session)
     }
 
-    array unset socks     $session
-    array unset buffers   $session
-    array unset bodies    $session
-    array unset requested $session
+    array unset socks   $session
+    array unset bodies  $session
+    array unset buffers $session
 
     return
 }
@@ -170,28 +182,74 @@ namespace eval ::tanzer::scgi::handler {
 }
 
 ::oo::define ::tanzer::scgi::handler method read {session data} {
-    my variable socks bodies
-
-    if {[array get socks $session] eq {}} {
-        my open $session
-    }
-
-    append bodies($session) $data
-}
-
-::oo::define ::tanzer::scgi::handler method write {session} {
-    my variable socks buffers requested
+    my variable socks bodies requested
 
     #
-    # If we have not forwarded the HTTP request to the SCGI service, then do
+    # Open a connection to the remote SCGI service if we have not already done
     # so.
+    #
+    if {[array get socks $session] eq {}} {
+        my open $session
+
+        return
+    }
+
+    #
+    # If we have data to buffer for the request body, then let's buffer that
+    # and quickly return.
+    #
+    if {[string length $data] > 0} {
+        append bodies($session) $data
+
+        return
+    }
+
+    #
+    # If we have no more data, and we have not sent our request to the remote
+    # SCGI service, then do so.
     #
     if {!$requested($session)} {
         puts -nonewline $socks($session) [my encodeRequest $session]
         flush $socks($session)
 
         set requested($session) 1
+
+        return
     }
+
+    #
+    # If we've already sent everything out for the buffered session body, then
+    # we can bind our writer to readable events on the SCGI socket.  Of course,
+    # the server will only know about the originating client socket, so we'll
+    # have to use that as reference for our current request handler.
+    #
+    if {[string length $bodies($session)] == 0} {
+        set server [$session server]
+        set sock   [$session sock]
+
+        chan event $socks($session) readable [list $server respond write $sock]
+        chan event $socks($session) writable {}
+
+        return
+    }
+
+    #
+    # Otherwise, let's send the request body to the SCGI service in chunks.
+    #
+    set size [$session config readlen]
+
+    puts -nonewline $socks($session) [string range $bodies($session) \
+        0 [expr {$size - 1}]]
+
+    #
+    # Truncate that amount off the beginning of the body.
+    #
+    set bodies($session) [string range $bodies($session) \
+        $size end]
+}
+
+::oo::define ::tanzer::scgi::handler method write {session} {
+    my variable socks buffers
 
     set size [$session config readsize]
     set sock [$session sock]
@@ -202,6 +260,9 @@ namespace eval ::tanzer::scgi::handler {
     # funneling data from the SCGI socket to the client socket.
     #
     if {[$session responded]} {
+        chan event $socks($session) readable {}
+        chan event $socks($session) writable {}
+
         my pipe $socks($session) $sock $session
 
         return
